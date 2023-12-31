e37b39f4588b8ea76af3fa86ea45b9aed7b37b3b
davidoodugama <davidoodugama1999@gmail.com>
2023-12-31 20:55:36 +0530

    apparel export done

--- dynamic.py
+++ dynamic.py
@@ -15,7 +15,7 @@ from urllib.parse import unquote
 from selenium.webdriver.common.by import By
 
 # session = HTMLSession()
-options               = Options()
+options = Options()
 options.add_argument('start-maximized')
 options.add_argument("--no-sandbox")
 # options.add_argument("--disable-dev-shm-usage")
@@ -24,112 +24,175 @@ options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option("detach", True)
 options.add_experimental_option("useAutomationExtension", False)
 
-def clickButton(loadMoreButtons, driver):
-    try:
-        time.sleep(2)
-        loadMoreButtons.click()
-
-        # Wait for the page to load after the click
-        WebDriverWait(driver, 15).until(
-            EC.staleness_of(loadMoreButtons)
-        )
-    except Exception as e:
-        print(f"Error clicking button: {e}")
-    
-    return driver.page_source
-
-def extract_table_info(table_rows, headers, type):
-    if type == 'export':
-        for rows in table_rows:
-            tds                                  = rows.find_all('td')
-            code                                 = tds[1].text.strip().replace(',', '')
-            product_label                        = tds[2].text.strip().replace(',', '')
-            export_value                         = tds[3].text.strip().replace(',', '')
-            trade_balance                        = tds[4].text.strip().replace(',', '')
-            annual_growth_1                      = tds[5].text.strip().replace(',', '')
-            annual_growth_2                      = tds[6].text.strip().replace(',', '')
-            annual_growth_of_world               = tds[7].text.strip().replace(',', '')
-            share_in_world                       = tds[8].text.strip().replace(',', '')
-            ranking_in_world                     = tds[9].text.strip().replace(',', '')
-            average_distance_of_supply_countries = tds[10].text.strip().replace(',', '')
-            concentration_of_supplying_countries = tds[11].text.strip().replace(',', '')
-            
-            print(f"""
-            Code: {code}
-            Product Label: {product_label}
-            {headers[0]}: {export_value}
-            {headers[1]}: {trade_balance}
-            {headers[2]}: {annual_growth_1}
-            {headers[3]}: {annual_growth_2}
-            {headers[4]}: {annual_growth_of_world}
-            {headers[5]}: {share_in_world}
-            {headers[6]}: {ranking_in_world}
-            {headers[7]}: {average_distance_of_supply_countries}
-            {headers[8]}: {concentration_of_supplying_countries}
-            """)
-    
-driver_path = ChromeDriverManager().install()
-soups = []
-# /root/.wdm/drivers/chromedriver/linux64/120.0.6099.109/chromedriver-linux64/chromedriver
+driver_path    = ChromeDriverManager().install()
 chrome_service = Service(executable_path=str(driver_path))
 
-driver              = webdriver.Chrome(service=chrome_service, options= options)
-url                 = 'https://www.trademap.org/Product_SelProductCountry.aspx?nvpm=1%7c144%7c%7c%7c%7cTOTAL%7c%7c%7c2%7c1%7c1%7c2%7c1%7c%7c1%7c1%7c1%7c1'
-decoded_url         = unquote(url)
+driver         = webdriver.Chrome(service=chrome_service, options= options)
+url            = 'https://tradingeconomics.com/'
+matrix_url     = 'https://tradingeconomics.com/matrix'
+crypto_url     = 'https://tradingeconomics.com/crypto'
 
-try:
-    driver.get(decoded_url)
-    driver.implicitly_wait(10)
-except:
-    driver.close()
-    driver = webdriver.Chrome(service=chrome_service, options= options)
-    
-    driver.get(decoded_url)
-    while driver.page_source == None:
-        driver.get(decoded_url)
-        driver.implicitly_wait(10)
+driver.get(url)
+driver.implicitly_wait(10)
 
 page_source  = driver.page_source
-pages        = []
 
-def getElement():
-    div_container = driver.find_element(By.ID, 'div_container')
-    tables        = div_container.find_elements(By.TAG_NAME,'table')
-    target_table  = tables[-4]
-    tbody         = target_table.find_element(By.TAG_NAME,'tbody')
-    tr            = tbody.find_element(By.XPATH,'//tr[@align="right" and @style="color:White;background-color:#86B3E0;font-size:80%;"]')
-    td_table      = tr.find_element(By.TAG_NAME,'td').find_element(By.TAG_NAME,'table')
-    td_tbody      = td_table.find_element(By.TAG_NAME,'tbody')
-    td_tr         = td_tbody.find_element(By.TAG_NAME,'tr')
-    
-    return td_tr
+driver.get(matrix_url)
+driver.implicitly_wait(10)
 
-td_tr = getElement()
-tds   = td_tr.find_elements(By.TAG_NAME,'td')[1:]
-id    = 1
-for td in tds:
-    try:
-        td.get_attribute("innerHTML")
-        pages.append(BeautifulSoup(clickButton(td, driver), 'lxml'))
-        id += 1
-    except:
-        td_tr = getElement()
-        td_1 = td_tr.find_elements(By.TAG_NAME,'td')[id]
-        pages.append(BeautifulSoup(clickButton(td_1, driver), 'lxml'))
-        id += 1
+page_source_matrix  = driver.page_source
 
+driver.get(crypto_url)
+driver.implicitly_wait(10)
+
+page_source_crypto  = driver.page_source
 driver.close()
-soup       = BeautifulSoup(page_source, 'lxml')
-table_rows = soup.find('div', attrs= {'id' :'div_container'}).find_all('table')[-4].tbody.find_all('tr', attrs= {"style": "color:White;background-color:#5D7B9D;font-size:80%;font-weight:bold;"})[1].find_all('th')
-headers = []
-for header in table_rows:
-    headers.append(header.a.text)
 
-# EXTRACT FIRST PAGE
-table_rows = soup.find('div', attrs= {'id' :'div_container'}).find_all('table')[-4].tbody.find_all('tr', attrs= {"align": "right"})[2:-1]
-extract_table_info(table_rows, headers, 'export')
+soup         = BeautifulSoup(page_source, 'lxml')
+soup_matrix  = BeautifulSoup(page_source_matrix, 'lxml')
+soup_crypto  = BeautifulSoup(page_source_crypto, 'lxml')
 
-# EXTRACT REST OF THE PAGES
-for page in pages:
-    table_rows = page.find('div', attrs= {'id' :'div_container'}).find_all('table')[-4].tbody.find_all('tr', attrs= {"align": "right"})[2:-1]
-    extract_table_info(table_rows, headers, 'export')
+# CARD INFORMATION
+print("")
+print(" ----------------------------------------------------------- CARD INFO ----------------------------------------------------------- ")
+cards = soup.find('div', class_= 'row', attrs={"id":"hometiles"}).find_all('div', class_= 'col-lg-4 col-md-6 col-xs-12')
+for card in cards:
+    heading      = card.find('div', class_= 'home-tile-inside').b.text.strip()
+    divs         = card.find('div', class_= 'home-tile-inside').find_all('div')
+    message      = divs[0].text.strip().strip()
+    updated_time = divs[1].text.strip().strip()
+    print(f"""
+    Heading     : {heading}
+    Message     : {message}
+    Updated Time: {updated_time}
+    """)
+
+# CALENDAR INFORMATION
+print("")
+print(" ----------------------------------------------------------- CALENDAR INFO ----------------------------------------------------------- ")
+calendar_data_rows = soup.find('div', class_= 'calendar-widget thumbnail').find_all('div', class_= 'calendar-widget-event')
+for row in calendar_data_rows:
+    time_   = ''.join(row.find('span', class_= 'calendar-widget-time').find_all(string=True, recursive=False)).strip()
+    message = ' '.join(row.a.span.text.strip().split())
+    print(f"""
+    Time    : {time_}
+    Message : {message}
+    """)
+
+# GDP INFORMATION
+print("")
+print(" ----------------------------------------------------------- GDP INFO ----------------------------------------------------------- ")
+gdp_info_table_rows = soup_matrix.find('div', class_= 'card').find('table', class_= 'table table-hover sortable-theme-minimal table-heatmap').tbody.find_all('tr')
+for row in gdp_info_table_rows:
+    tds             = row.find_all('td')
+    country         = tds[0].text.strip()
+    gdp             = tds[1].text.strip()
+    gdp_growth      = tds[2].text.strip()
+    interst_rate    = tds[3].text.strip()
+    inflation_rate  = tds[4].text.strip()
+    jobless_rate    = tds[5].text.strip()
+    gov_budget      = tds[6].text.strip()
+    debt_gdp        = tds[7].text.strip()
+    current_account = tds[8].text.strip()
+    population      = tds[9].text.strip()
+    
+    print(f"""
+    Country           : {country}
+    GDP               : {gdp}
+    GDP Growth        : {gdp_growth}
+    Interst Rate      : {interst_rate}
+    Inflation Rate    : {inflation_rate}
+    Jobless Rate      : {jobless_rate}
+    Government Budget : {gov_budget}
+    Debt/GDP          : {debt_gdp}
+    Current Account   : {current_account}
+    Population        : {population}
+    """)
+    
+div_cards = soup_crypto.find('div', class_= 'col-xl-12').find_all('div', class_= 'card')
+
+# CRYPTO
+print("")
+print(" ----------------------------------------------------------- CRYPTO ----------------------------------------------------------- ")
+crypto_info_table_rows = div_cards[0].find('table', class_= 'table table-hover table-striped table-heatmap').tbody.find_all('tr')
+for row in crypto_info_table_rows:
+    tds               = row.find_all('td')
+    crypto            = tds[0].text.strip()
+    price             = tds[1].text.strip()
+    day               = tds[2].text.strip()
+    day_change_status = tds[2].span.get('class')[0].split('-')[1]
+    percentage        = tds[3].text.strip()
+    weekly            = tds[4].text.strip()
+    monthly           = tds[5].text.strip()
+    yoy               = tds[6].text.strip()
+    market_cap        = tds[7].text.strip()
+    date              = tds[8].text.strip()
+    
+    print(f"""
+    Crypto            : {crypto}
+    Price             : {price}
+    Day               : {day}
+    Day Change Status : {day_change_status}
+    Percentage        : {percentage}
+    Weekly            : {weekly}
+    Monthly           : {monthly}
+    YoY               : {yoy}
+    Market Cap        : {market_cap}
+    Date              : {date}
+    """)
+
+# BTC
+print("")
+print(" ----------------------------------------------------------- BTC ----------------------------------------------------------- ")
+btc_info_table_rows   = div_cards[1].find('table', class_= 'table table-hover table-striped table-heatmap').tbody.find_all('tr')
+for row in btc_info_table_rows:
+    tds               = row.find_all('td')
+    btc               = tds[0].text.strip()
+    price             = tds[1].text.strip()
+    day               = tds[2].text.strip()
+    day_change_status = tds[2].span.get('class')[0].split('-')[1]
+    percentage        = tds[3].text.strip()
+    weekly            = tds[4].text.strip()
+    monthly           = tds[5].text.strip()
+    yoy               = tds[6].text.strip()
+    date              = tds[7].text.strip()
+    
+    print(f"""
+    BTC               : {btc}
+    Price             : {price}
+    Day               : {day}
+    Day Change Status : {day_change_status}
+    Percentage        : {percentage}
+    Weekly            : {weekly}
+    Monthly           : {monthly}
+    YoY               : {yoy}
+    Date              : {date}
+    """)
+
+# ETH
+print("")
+print(" ----------------------------------------------------------- ETH ----------------------------------------------------------- ")
+eth_info_table_rows   = div_cards[2].find('table', class_= 'table table-hover table-striped table-heatmap').tbody.find_all('tr')
+for row in eth_info_table_rows:
+    tds               = row.find_all('td')
+    eth               = tds[0].text.strip()
+    price             = tds[1].text.strip()
+    day               = tds[2].text.strip()
+    day_change_status = tds[2].span.get('class')[0].split('-')[1]
+    percentage        = tds[3].text.strip()
+    weekly            = tds[4].text.strip()
+    monthly           = tds[5].text.strip()
+    yoy               = tds[6].text.strip()
+    date              = tds[7].text.strip()
+    
+    print(f"""
+    ETH               : {eth}
+    Price             : {price}
+    Day               : {day}
+    Day Change Status : {day_change_status}
+    Percentage        : {percentage}
+    Weekly            : {weekly}
+    Monthly           : {monthly}
+    YoY               : {yoy}
+    Date              : {date}
+    """)
--- scrap.py
+++ scrap.py
@@ -1188,5 +1188,178 @@ def import_export_data(url, type):
         table_rows = page.find('div', attrs= {'id' :'div_container'}).find_all('table')[-4].tbody.find_all('tr', attrs= {"align": "right"})[2:-1]
         extract_table_info(table_rows, headers, type)
 
+def apparel_exports():
+    global driver_path
+    chrome_service            = Service(executable_path=str(driver_path))
+    driver                    = webdriver.Chrome(service=chrome_service, options= options)
+    url            = 'https://tradingeconomics.com/'
+    matrix_url     = 'https://tradingeconomics.com/matrix'
+    crypto_url     = 'https://tradingeconomics.com/crypto'
+
+    driver.get(url)
+    driver.implicitly_wait(10)
+
+    page_source  = driver.page_source
+
+    driver.get(matrix_url)
+    driver.implicitly_wait(10)
+
+    page_source_matrix  = driver.page_source
+
+    driver.get(crypto_url)
+    driver.implicitly_wait(10)
+
+    page_source_crypto  = driver.page_source
+    driver.close()
+
+    soup         = BeautifulSoup(page_source, 'lxml')
+    soup_matrix  = BeautifulSoup(page_source_matrix, 'lxml')
+    soup_crypto  = BeautifulSoup(page_source_crypto, 'lxml')
+
+    # CARD INFORMATION
+    print("")
+    print(" ----------------------------------------------------------- CARD INFO ----------------------------------------------------------- ")
+    cards = soup.find('div', class_= 'row', attrs={"id":"hometiles"}).find_all('div', class_= 'col-lg-4 col-md-6 col-xs-12')
+    for card in cards:
+        heading      = card.find('div', class_= 'home-tile-inside').b.text.strip()
+        divs         = card.find('div', class_= 'home-tile-inside').find_all('div')
+        message      = divs[0].text.strip().strip()
+        updated_time = divs[1].text.strip().strip()
+        print(f"""
+        Heading     : {heading}
+        Message     : {message}
+        Updated Time: {updated_time}
+        """)
+
+    # CALENDAR INFORMATION
+    print("")
+    print(" ----------------------------------------------------------- CALENDAR INFO ----------------------------------------------------------- ")
+    calendar_data_rows = soup.find('div', class_= 'calendar-widget thumbnail').find_all('div', class_= 'calendar-widget-event')
+    for row in calendar_data_rows:
+        time_   = ''.join(row.find('span', class_= 'calendar-widget-time').find_all(string=True, recursive=False)).strip()
+        message = ' '.join(row.a.span.text.strip().split())
+        print(f"""
+        Time    : {time_}
+        Message : {message}
+        """)
+
+    # GDP INFORMATION
+    print("")
+    print(" ----------------------------------------------------------- GDP INFO ----------------------------------------------------------- ")
+    gdp_info_table_rows = soup_matrix.find('div', class_= 'card').find('table', class_= 'table table-hover sortable-theme-minimal table-heatmap').tbody.find_all('tr')
+    for row in gdp_info_table_rows:
+        tds             = row.find_all('td')
+        country         = tds[0].text.strip()
+        gdp             = tds[1].text.strip()
+        gdp_growth      = tds[2].text.strip()
+        interst_rate    = tds[3].text.strip()
+        inflation_rate  = tds[4].text.strip()
+        jobless_rate    = tds[5].text.strip()
+        gov_budget      = tds[6].text.strip()
+        debt_gdp        = tds[7].text.strip()
+        current_account = tds[8].text.strip()
+        population      = tds[9].text.strip()
+        
+        print(f"""
+        Country           : {country}
+        GDP               : {gdp}
+        GDP Growth        : {gdp_growth}
+        Interst Rate      : {interst_rate}
+        Inflation Rate    : {inflation_rate}
+        Jobless Rate      : {jobless_rate}
+        Government Budget : {gov_budget}
+        Debt/GDP          : {debt_gdp}
+        Current Account   : {current_account}
+        Population        : {population}
+        """)
+        
+    div_cards = soup_crypto.find('div', class_= 'col-xl-12').find_all('div', class_= 'card')
+
+    # CRYPTO
+    print("")
+    print(" ----------------------------------------------------------- CRYPTO ----------------------------------------------------------- ")
+    crypto_info_table_rows = div_cards[0].find('table', class_= 'table table-hover table-striped table-heatmap').tbody.find_all('tr')
+    for row in crypto_info_table_rows:
+        tds               = row.find_all('td')
+        crypto            = tds[0].text.strip()
+        price             = tds[1].text.strip()
+        day               = tds[2].text.strip()
+        day_change_status = tds[2].span.get('class')[0].split('-')[1]
+        percentage        = tds[3].text.strip()
+        weekly            = tds[4].text.strip()
+        monthly           = tds[5].text.strip()
+        yoy               = tds[6].text.strip()
+        market_cap        = tds[7].text.strip()
+        date              = tds[8].text.strip()
+        
+        print(f"""
+        Crypto            : {crypto}
+        Price             : {price}
+        Day               : {day}
+        Day Change Status : {day_change_status}
+        Percentage        : {percentage}
+        Weekly            : {weekly}
+        Monthly           : {monthly}
+        YoY               : {yoy}
+        Market Cap        : {market_cap}
+        Date              : {date}
+        """)
+
+    # BTC
+    print("")
+    print(" ----------------------------------------------------------- BTC ----------------------------------------------------------- ")
+    btc_info_table_rows   = div_cards[1].find('table', class_= 'table table-hover table-striped table-heatmap').tbody.find_all('tr')
+    for row in btc_info_table_rows:
+        tds               = row.find_all('td')
+        btc               = tds[0].text.strip()
+        price             = tds[1].text.strip()
+        day               = tds[2].text.strip()
+        day_change_status = tds[2].span.get('class')[0].split('-')[1]
+        percentage        = tds[3].text.strip()
+        weekly            = tds[4].text.strip()
+        monthly           = tds[5].text.strip()
+        yoy               = tds[6].text.strip()
+        date              = tds[7].text.strip()
+        
+        print(f"""
+        BTC               : {btc}
+        Price             : {price}
+        Day               : {day}
+        Day Change Status : {day_change_status}
+        Percentage        : {percentage}
+        Weekly            : {weekly}
+        Monthly           : {monthly}
+        YoY               : {yoy}
+        Date              : {date}
+        """)
+
+    # ETH
+    print("")
+    print(" ----------------------------------------------------------- ETH ----------------------------------------------------------- ")
+    eth_info_table_rows   = div_cards[2].find('table', class_= 'table table-hover table-striped table-heatmap').tbody.find_all('tr')
+    for row in eth_info_table_rows:
+        tds               = row.find_all('td')
+        eth               = tds[0].text.strip()
+        price             = tds[1].text.strip()
+        day               = tds[2].text.strip()
+        day_change_status = tds[2].span.get('class')[0].split('-')[1]
+        percentage        = tds[3].text.strip()
+        weekly            = tds[4].text.strip()
+        monthly           = tds[5].text.strip()
+        yoy               = tds[6].text.strip()
+        date              = tds[7].text.strip()
+        
+        print(f"""
+        ETH               : {eth}
+        Price             : {price}
+        Day               : {day}
+        Day Change Status : {day_change_status}
+        Percentage        : {percentage}
+        Weekly            : {weekly}
+        Monthly           : {monthly}
+        YoY               : {yoy}
+        Date              : {date}
+        """)
+
 # import_export_data('https://www.trademap.org/Product_SelProductCountry.aspx?nvpm=1%7c144%7c%7c%7c%7cTOTAL%7c%7c%7c2%7c1%7c1%7c1%7c1%7c1%7c1%7c1%7c1%7c1', 'import')
 # import_export_data('https://www.trademap.org/Product_SelProductCountry.aspx?nvpm=1%7c144%7c%7c%7c%7cTOTAL%7c%7c%7c2%7c1%7c1%7c2%7c1%7c%7c1%7c1%7c1%7c1', 'export')
